b775d6a2d04ef9661a704d9c47fd770b2e275ecb
Vinicius Pizetta de Souza <viniciuspzt@gmail.com>
2020-03-18 17:59:40 -0300

    Intro

---
+++ IntroToVueJs/2/main.js
@@ -0,0 +1,10 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        image: './assets/vmSocks-green.jpg',
+        altText: 'A pair of socks',
+        url: 'www.google.com'
+    }
+})
---
+++ IntroToVueJs/3/main.js
@@ -0,0 +1,12 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A pair of warm, fuzzy socks',
+        image: './assets/vmSocks-green.jpg',
+        altText: 'A pair of socks',
+        url: 'www.google.com',
+        inventory: 5,
+        onSale: true
+    }
+})
